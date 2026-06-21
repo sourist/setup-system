cba66ffbd3237dd880f76affdea1ad96cc90e004
Danila Hetman <danil46870@gmail.com>
2026-06-21 15:19:32 +0300

    feat(neovim): colorcolumn

--- configs/nvim/lua/config/core.lua
+++ configs/nvim/lua/config/core.lua
@@ -10,6 +10,7 @@ vim.opt.clipboard = "unnamedplus"
 vim.g.mapleader = " "
 vim.g.maplocalleader = ","
 vim.opt.swapfile = false
+vim.opt.colorcolumn = "90"
 
 vim.opt.langmap = 'ФИСВУАПРШОЛДЬТЩЗЙКЫЕГМЦЧНЯ;ABCDEFGHIJKLMNOPQRSTUVWXYZ,фисвуапршолдьтщзйкыегмцчня;abcdefghijklmnopqrstuvwxyz'
 vim.keymap.set('n', 'Ж', ':', { noremap = true })
